8a845bb39480a2256e9c15171b9d4dd149f24bc0
Michael Mendoza <michael.mend97@yahoo.com>
2020-02-02 17:55:02 -0500

    fixed save button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var timeBlock = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM']
 
 for (i = 0; i < timeBlock.length; i++) {
-    $('.container').append('<div class="row"\><div class="col-sm-1 sTime text-center">' + timeBlock[i] +'</div\><div class="col-sm-10"\><textarea id=' + 'txt' + i + ' cols="30" rows="4"> </textarea\></div\><div class="col-sm-1"><button id=' + i + '></button></div\></div>')
+    $('.container').append('<div class="row"\><div class="col-sm-1 sTime text-center" name='+ timeBlock[i] +'>' + timeBlock[i] +'</div\><div class="col-sm-10"\><textarea id=' + 'txt' + i + ' cols="30" rows="4"> </textarea\></div\><div class="col-sm-1"><button id=' + i + '></button></div\></div>')
 };
 
 $('#td').html(moment().format('MMMM Do YYYY h:mm a'));
@@ -9,6 +9,7 @@ setInterval(function(){
     $('#td').html(moment().format('MMMM Do YYYY h:mm a'));
 },30000);
 
-$('#' + timeBlock[i]).click(function(){
-        console.log($('#txt' + ));
-    });
+$('button').click(function(e){
+//    console.log( $('#txt0').val());
+    console.log(e.target);
+});
