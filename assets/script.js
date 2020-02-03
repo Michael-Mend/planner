d6b1c09c6c5cf50c412a264abf290f1b7832c91b
Michael Mendoza <michael.mend97@yahoo.com>
2020-02-03 04:44:10 -0500

    created past present and future display

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,23 @@ var timeBlock = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM']
 
 for (i = 0; i < timeBlock.length; i++) {
     $('.main').append('<div class="row"\><div class="col-sm-1 sTime text-center">' + timeBlock[i] +'</div\><div class="col-sm-10"\><textarea id=' + 'txt' + i + ' cols="30" rows="4"> </textarea\></div\><div class="col-sm-1"><button class="sv" id=' + i + '></button></div\></div>')
-};
-
-for (i = 0; i < timeBlock.length; i++) {
+    
     $('#txt' + i).text(localStorage.getItem(i));
+
+    if ($('#' + i).attr('id') + 0 > moment().format('H') - 9) {
+        $('#txt' + i).attr('class', 'future')
+    } else if ($('#' + i).attr('id') + 0 == moment().format('H') - 9) {
+        $('#txt' + i).attr('class', 'present')
+    } else if ($('#' + i).attr('id') < 0 == moment().format('H') - 9) {
+        $('#txt' + i).attr('class', 'past')
+    }
 };
 
-$('#td').html(moment().format('MMMM Do YYYY h:mm a'));
 setInterval(function(){
     $('#td').html(moment().format('MMMM Do YYYY h:mm a'));
 },30000);
+
+$('#td').html(moment().format('MMMM Do YYYY h:mm a'));
 
 $('.sv').click(function(e){
     localStorage.setItem(e.target.id, $('#txt' + e.target.id).val());
@@ -20,4 +27,4 @@ $('.sv').click(function(e){
 $('#clr').click(function(){
     $('textarea').text('')
     return localStorage.clear();
-})
+});
